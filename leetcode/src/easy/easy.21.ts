c24ce6ffdc4b12324f006844c16eecc2a1b33c4e
youngseok.jang <ys123.jang@gmail.com>
2025-02-22 10:49:45 +0900

    solution: LeetCode 21 easy - Merge Two Sorted Lists

---
+++ leetcode/src/easy/easy.21.ts
@@ -0,0 +1,33 @@
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+export function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null,
+): ListNode | null {
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  let head = new ListNode();
+  let current = head;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+  current.next = list1 ?? list2;
+
+  return head.next;
+}
--- leetcode/tests/solution.test.ts
+++ leetcode/tests/solution.test.ts
@@ -1,5 +1,53 @@
+import { ListNode, mergeTwoLists } from "../src/easy/easy.21";
 import { add } from "../src/easy/solution";
 
-test("add function works correctly", () => {
-  expect(add(2, 3)).toBe(5);
+describe("leetcode easy problem", () => {
+  test("add function works correctly", () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  describe("21. Merge two sorted lists", () => {
+    function createLinkedList(arr: number[]): ListNode | null {
+      if (arr.length === 0) return null;
+
+      let head = new ListNode(arr[0]);
+      let current = head;
+      for (let i = 0; i < arr.length; i++) {
+        current.next = new ListNode(arr[i]);
+        current = current.next;
+      }
+      return head.next;
+    }
+    function linkedListToArray(head: ListNode | null): number[] {
+      let arr: number[] = [];
+      while (head) {
+        arr.push(head.val);
+        head = head.next;
+      }
+      return arr;
+    }
+
+    test("Merge two lists.", () => {
+      const list1 = createLinkedList([1, 2, 4]);
+      const list2 = createLinkedList([1, 3, 4]);
+      const mergedList = mergeTwoLists(list1, list2);
+      console.log(linkedListToArray(mergedList));
+
+      expect(linkedListToArray(mergedList)).toEqual([1, 1, 2, 3, 4, 4]);
+    });
+
+    test("Merge two empty lists.", () => {
+      const list1 = createLinkedList([]);
+      const list2 = createLinkedList([]);
+      const mergedList = mergeTwoLists(list1, list2);
+      expect(linkedListToArray(mergedList)).toEqual([]);
+    });
+
+    test("Merge one empty list and one list.", () => {
+      const list1 = createLinkedList([]);
+      const list2 = createLinkedList([0]);
+      const mergedList = mergeTwoLists(list1, list2);
+      expect(linkedListToArray(mergedList)).toEqual([0]);
+    });
+  });
 });
